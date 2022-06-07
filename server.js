2f2ddec82b6e56bcb45d70cbd690090f80116c78
NSTaft <taftnathan13@gmail.com>
2022-06-07 12:46:03 -0500

    Created index, show, new, create route

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const fruitsSchema = new Schema({
     readyToEat: Boolean,
 })
 
-// Make Fruit model
+// Make Fruit model // the " " argument becomes a collection
 const Fruit = model("Fruit", fruitsSchema)
 
 
@@ -91,10 +91,51 @@ app.get("/fruits/seed", (req, res) => {
 });
 
 // Index route
-app.get('/fruits', async (req,res) => {
+app.get('/fruits', async (req, res) => {
     const fruits = await Fruit.find({})
-    res.render('fruits/index.liquid', {fruits})
+    res.render('fruits/index.liquid', { fruits })
 })
+
+// create route
+app.post("/fruits", (req, res) => {
+    // check if the readyToEat property should be true or false
+    req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
+    // create the new fruit
+    Fruit.create(req.body)
+        .then((fruits) => {
+            // redirect user to index page if successfully created item
+            res.redirect("/fruits");
+        })
+        // send error as json
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
+
+// New route // Edit button route
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.liquid')
+})
+
+// show route
+app.get("/fruits/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+
+    // find the particular fruit from the database
+    Fruit.findById(id)
+        .then((fruit) => {
+            // render the template with the data from the database
+            res.render("fruits/show.liquid", { fruit });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({ error });
+        });
+});
+
 
 
 //////////////////////////////////////////////
